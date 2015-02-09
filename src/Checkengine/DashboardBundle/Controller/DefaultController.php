16762d1b64b51575ad5f209abf9424a7edd46618
Richpolis Systems <richpolis@gmail.com>
2015-02-08 21:47:01 -0600

    Arreglo de method

--- src/Checkengine/DashboardBundle/Controller/DefaultController.php
+++ src/Checkengine/DashboardBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Checkengine\DashboardBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 use Checkengine\DashboardBundle\Entity\Enquiry;
 use Checkengine\DashboardBundle\Form\EnquiryType;
